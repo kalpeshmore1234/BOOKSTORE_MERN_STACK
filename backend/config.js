b51fb07f79e8aee4b5da3764ee85a696213eccbd
Kalpesh More <90396306+kalpeshmore1234@users.noreply.github.com>
2024-07-02 05:22:35 +0530

    add files

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 7000;
+
+export const mongoDBURL ='mongodb+srv://klapesh3869:ctvzy94JN2xDhx5b@book-store-mern.vfolj5l.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN'
